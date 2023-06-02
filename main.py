707976500d2a3be74c9cf33c4b1c824ad1cf640b
Memín27 <mateopalacios2727@gmail.com>
2023-06-02 12:54:05 -0500

    tablero

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ class Triqui:
         self.tablero = [[" " for _ in range(3)] for _ in range(3)]
         self.jugador_actual = "X"
         self.movimientos = 0
+        def mostrar_tablero(self):
+        print("-------------")
+        for i in range(3):
+            print("|", end=" ")
+            for j in range(3):
+                print(self.tablero[i][j], "|", end=" ")
+            print("\n-------------")
