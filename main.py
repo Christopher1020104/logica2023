f8bbdc6e7bf46ecd7e9da3117aa71c94347b5613
miguel16200523 <mahincapiesosa@gmail.com>
2023-06-07 10:40:00 -0500

    hello

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 class Triqui:
-    def _init_(self):
+    def __init__(self):
         self.tablero = [[" " for _ in range(3)] for _ in range(3)]
         self.jugador_actual = "X"
         self.movimientos = 0
